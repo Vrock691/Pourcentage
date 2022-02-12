7825e83e222cb2fa1c04c3318e6579f1555fea64
Vrock691 <vrock691@gmail.com>
2022-02-12 17:32:00 +0100

    empy

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pourcentage/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
   });
 }
